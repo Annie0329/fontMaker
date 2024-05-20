0238f86691a1eb77b8d90bda5131154512bc0d3c
Annie0329 <0329annie@gmail.com>
2024-05-20 14:40:55 +0800

    0520

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
 $(document).ready(function () {
 
   // 我不會JS，這是我自己看範例掰的，沒想到竟然成功
-  const $btn = $('#btn')
-
-  //按按鈕也可以
-  $('input[type=file]').change(function () {
-    var fileName = $('input[type=file]').val().split('\\').pop();
-    fileName = fileName.replace(".ttf", "")
-    $("#testFont").text(fileName)
-    $(".test").css("font-family", fileName);
+  $('#fontName').change(function () {
+    $("#testFont").text($('#fontName').val())
+    $(".test").css("font-family", $('#fontName').val());
   });
 
 });
